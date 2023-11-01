ad97a8f69ef5ba5bd229f633cec50d44b38a54d2
Capksz <emosker@gmail.com>
2023-11-01 02:22:43 -0400

    Added feed pet, more tests, and feed pet test

--- src/petmagotchi/pet.py
+++ src/petmagotchi/pet.py
@@ -2,19 +2,27 @@ import time
 import random
 
 class Pet:
+    class InvalidFoodError(Exception):
+        """Raised when an invalid food is given to the pet."""
+        pass
+
+    class InvalidQuantityError(Exception):
+        """Raised when the user gives too many or too little quantity"""
+    
     HUNGER_RATE = 3
     THIRST_RATE = 4
     CLEANLINESS_RATE = 1
     MOOD_RATE = 2
+    VALID_FOODS = {'meat':(15,5),'vegetable':(10,2),'ice cream':(5,15),'bread':(8,8)} #meal:(hunger fiil, mood increase)
     
     def __init__(self, name, type):
         if not isinstance(name, str) or not isinstance(type, str):
             raise ValueError("Name and type must be strings.")
         self.name = name
         self.type = type
-        self.food_level = random.randrange(55, 80)
-        self.water_level = random.randrange(55, 80)
-        self.sanitation_level = random.randrange(55, 80)
+        self.food_level = random.randint(55, 80)
+        self.water_level = random.randint(55, 80)
+        self.sanitation_level = random.randint(55, 80)
         self.mood_level = 90
         self.last_update_time = time.time()
 
@@ -30,17 +38,7 @@ class Pet:
         else:
             return f"{self.name} is excited and happy to be with you!"
 
-    def get_status(self):
-        self._update_status()
-        return {
-            "Pet": self.name,
-            "Mood": self.get_mood(),
-            "Hunger": self.food_level,
-            "Thirst": self.water_level,
-            "Cleanliness": self.sanitation_level
-        }
-
-    def _update_status(self):
+    def get_updated_status(self):
         time_change = time.time() - self.last_update_time
         rate_of_change = time_change / 1800
         self.food_level = max(self.food_level - rate_of_change * Pet.HUNGER_RATE, 0)
@@ -48,12 +46,44 @@ class Pet:
         self.sanitation_level = max(self.sanitation_level - rate_of_change * Pet.CLEANLINESS_RATE, 0)
         self.mood_level = max(self.mood_level - rate_of_change * Pet.MOOD_RATE, 0)
         self.last_update_time = time.time()
+        return {
+            "Pet": self.name,
+            "Mood": self.get_mood(),
+            "Hunger": self.food_level,
+            "Thirst": self.water_level,
+            "Cleanliness": self.sanitation_level
+        }
     
     def print_status(self):
-        status = self.get_status()
+        status = self.get_updated_status()
         order = ["Pet","Mood","Hunger","Thirst","Cleanliness"]
         for key in order:
             print(key+":",status[key])
+
+    def feed_pet(self,food,quantity):
+        self.get_updated_status()
+        if food not in Pet.VALID_FOODS:
+            raise Pet.InvalidFoodError(f"'{food}' is not a valid food. Valid foods are: {', '.join(Pet.VALID_FOODS)}")
+        if quantity>3 or quantity<1:
+            raise Pet.InvalidQuantityError(f"'{quantity}' is not a valid quantity. Valid quantities are 1,2 and 3. Can't give your pet a stomachache!")
+        if not isinstance(food,str) or not isinstance(quantity,int):
+            raise ValueError("Food must be a string and quantity must be an int.")
+        food_fill = Pet.VALID_FOODS[food][0]
+        mood_increase = Pet.VALID_FOODS[food][1]
+        self.food_level = min(self.food_level + (food_fill * quantity),100)
+        self.mood_level = min(self.mood_level + (mood_increase * quantity),100)
+
+        reactions = {
+            "meat": f"{self.name} pounces on the steak and gobbles it up!",
+            "vegetable": f"{self.name} finished the fragrant vegetables before you knew it!",
+            "ice cream": f"The sweet treat draws a smile on {self.name}'s face! He appreciates that you wiped the leftover cream from his mouth.",
+            "bread": f"Perfect toasty sandwich! A comfortable meal for {self.name}."
+        }
+        
+        print(reactions.get(food, f"{self.name} enjoyed the meal!"))
+
+
+        
 
 
 
--- tests/test_create_pet_and_status.py
+++ tests/test_create_pet_and_status.py
@@ -1,4 +1,6 @@
 import pytest
+import random
+from io import StringIO
 from petmagotchi.pet import Pet
 from unittest.mock import patch
 
@@ -6,6 +8,7 @@ class TestPet:
     def setup_method(self):
         self.pet = Pet(name="Yuka", type="Cat")
     
+    #tests constructor
     def test_pet_creation(self):
         assert isinstance(self.pet.name,str)
         assert isinstance(self.pet.type,str)
@@ -13,7 +16,29 @@ class TestPet:
         assert 55 <= self.pet.water_level <= 80
         assert 55 <= self.pet.sanitation_level <= 80
         assert self.pet.mood_level == 90
+
+    def test_invalid_pet_creation(self):
+        with pytest.raises(ValueError):
+            Pet(name=123,type="Cat")
+        with pytest.raises(ValueError):
+            Pet(name="Yuka",type=123)
     
+    def test_pet_creation_boundary_values(self):
+        pets = [Pet(name="Test",type="Type") for _ in range(1000)]
+        food_levels = [pet.food_level for pet in pets]
+        water_levels = [pet.water_level for pet in pets]
+        sanitation_levels = [pet.sanitation_level for pet in pets]
+        mood_levels = [pet.mood_level for pet in pets]
+        assert min(food_levels) == 55
+        assert max(food_levels) == 80 
+        assert min(water_levels) == 55
+        assert max(water_levels) == 80
+        assert min(sanitation_levels) == 55
+        assert max(sanitation_levels) == 80
+        assert min(mood_levels) == 90
+        assert max(mood_levels) == 90
+    
+    #tests get_mood
     def test_mood_status(self):
         self.pet.mood_level = 10
         assert self.pet.get_mood() == f"{self.pet.name} is sad and lonely..."
@@ -29,19 +54,129 @@ class TestPet:
 
         self.pet.mood_level = 90
         assert self.pet.get_mood() == f"{self.pet.name} is excited and happy to be with you!"
+
+    def test_mood_boundaries(self):
+        boundaries = [0, 19, 20, 39, 40, 59, 60, 79, 80, 100]
+        expected_responses = [
+            f"{self.pet.name} is sad and lonely...",
+            f"{self.pet.name} is sad and lonely...",
+            f"{self.pet.name} is sad...",
+            f"{self.pet.name} is sad...",
+            f"{self.pet.name} would like some more attention...",
+            f"{self.pet.name} would like some more attention...",
+            f"{self.pet.name} is happy!",
+            f"{self.pet.name} is happy!",
+            f"{self.pet.name} is excited and happy to be with you!",
+            f"{self.pet.name} is excited and happy to be with you!"
+        ]
+        for i, boundary in enumerate(boundaries):
+            self.pet.mood_level = boundary
+            assert self.pet.get_mood() == expected_responses[i]
     
-    def test_update_status_time_shift(self):
+    def test_randomized_mood_status(self):
+        mood_ranges = {
+            (0, 19): f"{self.pet.name} is sad and lonely...",
+            (20, 39): f"{self.pet.name} is sad...",
+            (40, 59): f"{self.pet.name} would like some more attention...",
+            (60, 79): f"{self.pet.name} is happy!",
+            (80, 100): f"{self.pet.name} is excited and happy to be with you!"
+        }
+
+        for mood_range, expected_mood in mood_ranges.items():
+            for i in range(1000):
+                random_mood = random.randint(mood_range[0], mood_range[1])
+                self.pet.mood_level = random_mood
+                assert self.pet.get_mood() == expected_mood
+
+
+    #Test get_updated_status
+    def test_update_status_value_decrease(self):
         starting_time = 0
         jump = 3600
-        with patch('time.time', return_value = starting_time):
-            pet = Pet(name="Yuka", type="Cat")
-            inital_status = pet.get_status()
+        with patch('time.time', return_value=starting_time):
+            self.pet = Pet(name="Yuka", type="Cat")
+            initial_status = self.pet.get_updated_status()
+            initial_food = self.pet.food_level
+            initial_water = self.pet.water_level
+            initial_sanitation = self.pet.sanitation_level
+            initial_mood = self.pet.mood_level
+
+        with patch('time.time', return_value=starting_time + jump):
+            updated_status = self.pet.get_updated_status()
+            updated_food = self.pet.food_level
+            updated_water = self.pet.water_level
+            updated_sanitation = self.pet.sanitation_level
+            updated_mood = self.pet.mood_level
+
+        assert updated_food < initial_food
+        assert updated_water < initial_water
+        assert updated_sanitation < initial_sanitation
+        assert updated_mood < initial_mood
+        assert updated_status["Hunger"] < initial_status["Hunger"]
+        assert updated_status["Thirst"] < initial_status["Thirst"]
+        assert updated_status["Cleanliness"] < initial_status["Cleanliness"]
         
-        with patch('time.time', return_value = starting_time + jump):
-            updated_status = pet.get_status()
-        
-        assert updated_status["Hunger"] < inital_status["Hunger"]
-        assert updated_status["Thirst"] < inital_status["Thirst"]
-        assert updated_status["Cleanliness"] < inital_status["Cleanliness"]
+
+    def test_exact_status_update_values(self):
+        starting_time = 0
+        jump = 3600  # 1 hour
+        with patch('time.time', return_value=starting_time):
+            self.pet = Pet(name="Yuka", type="Cat")
+            initial_food = self.pet.food_level
+            initial_water = self.pet.water_level
+            initial_sanitation = self.pet.sanitation_level
+            initial_mood = self.pet.mood_level
+
+        expected_food_change = (jump/1800) * Pet.HUNGER_RATE
+        expected_water_change = (jump/1800) * Pet.THIRST_RATE
+        expected_sanitation_change = (jump/1800) * Pet.CLEANLINESS_RATE
+        expected_mood_change = (jump/1800) * Pet.MOOD_RATE
+
+        with patch('time.time', return_value=starting_time + jump):
+            updated_status = self.pet.get_updated_status()
+
+        assert self.pet.food_level == max(initial_food - expected_food_change, 0)
+        assert self.pet.water_level == max(initial_water - expected_water_change, 0)
+        assert self.pet.sanitation_level == max(initial_sanitation - expected_sanitation_change, 0)
+        assert self.pet.mood_level == max(initial_mood - expected_mood_change, 0)
+        assert updated_status["Hunger"] == max(initial_food - expected_food_change, 0)
+        assert updated_status["Thirst"] == max(initial_water - expected_water_change, 0)
+        assert updated_status["Cleanliness"] == max(initial_sanitation - expected_sanitation_change, 0)
+
+    def test_minimum_status_values_after_large_time_jump(self):
+        starting_time = 0
+        large_jump = 10000000
+        with patch('time.time', return_value=starting_time):
+            self.pet = Pet(name="Yuka", type="Cat")
+
+        with patch('time.time', return_value=starting_time + large_jump):
+            updated_status = self.pet.get_updated_status()
+            updated_mood = self.pet.mood_level
+
+        assert updated_status["Hunger"] == 0
+        assert updated_status["Thirst"] == 0
+        assert updated_status["Cleanliness"] == 0
+        assert self.pet.mood_level == 0
+        assert updated_status["Mood"] == f"{self.pet.name} is sad and lonely..."
+
+    #test print status
+    def test_print_status(self):
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            self.pet.print_status()
+            output = mock_stdout.getvalue()
+
+        lines = output.strip().split("\n")
+
+        expected_output = [
+            "Pet: Yuka",
+            f"Mood: {self.pet.get_updated_status()['Mood']}",
+            f"Hunger: {self.pet.get_updated_status()['Hunger']}",
+            f"Thirst: {self.pet.get_updated_status()['Thirst']}",
+            f"Cleanliness: {self.pet.get_updated_status()['Cleanliness']}"
+        ]
+
+        for i in range(len(lines)):
+            assert lines[i] == expected_output[i]
+
 
         
