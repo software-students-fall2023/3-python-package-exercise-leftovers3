a4081fef10bd0213735a6b5b96c238723d1b42a8
BradFeng02 <bradfeng02@gmail.com>
2023-11-05 03:09:11 -0500

    bring_to update stats tests

--- tests/test_bring_to.py
+++ tests/test_bring_to.py
@@ -43,23 +43,71 @@ def pet() -> Pet:
         yield pet0
 
 
+# side effect to set stats
+def set_stats(p: Pet, mood_level=0, sanitation_level=0, energy_level=0, food_level=0, water_level=0):
+    def p_set_stats():
+        p.mood_level = mood_level
+        p.sanitation_level = sanitation_level
+        p.energy_level = energy_level
+        p.food_level = food_level
+        p.water_level = water_level
+
+    return p_set_stats
+
+
+# patch _update_status to set stats
+def set_stats_on_update(p: Pet, mood_level=0, sanitation_level=0, energy_level=0, food_level=0, water_level=0):
+    return patch.object(Pet, '_update_status',
+        side_effect=set_stats(p, mood_level, sanitation_level, energy_level, food_level, water_level))
+
+
+# assert expected stats
+def expect_stats(p: Pet, mood_level, sanitation_level, energy_level, food_level, water_level):
+    assert p.mood_level == mood_level
+    assert p.sanitation_level == sanitation_level
+    assert p.energy_level == energy_level
+    assert p.food_level == food_level
+    assert p.water_level == water_level
+
+
 class TestBringTo():
 
     def test_bring_to_bad_destination(self, pet: Pet):
         with pytest.raises(ValueError):
             pet.bring_to(123)
     
+
     def test_bring_to_invalid_destination(self, pet: Pet):
         with pytest.raises(Pet.InvalidDestinationError):
             pet.bring_to('movies')
 
+
     def test_bring_to_lower(self, pet: Pet):
         assert pet.bring_to('PaRk') == 'test Moana park'
+
 
     def test_bring_to_travel_cooldown(self, pet: Pet):
         with patch('time.time', return_value=10):
             assert pet.bring_to('park') == 'test Moana park'
+            
         with patch('time.time', return_value=50):
-            assert pet.bring_to('park') == "Moana wants to relax at home. Try waiting some time before bringing them out again!"
-        with patch('time.time', return_value=150):
+            assert pet.bring_to('hike') == "Moana wants to relax at home. Try waiting some time before bringing them out again!"
+
+        with patch('time.time', return_value=110):
             assert pet.bring_to('hike') == 'test Moana hike'
+
+
+    def test_bring_to_update_stats(self, pet: Pet):
+        with patch('time.time', return_value=0), set_stats_on_update(pet):
+            assert pet.bring_to('park') == 'test Moana park'
+            expect_stats(pet, 10, 20, 30, 40, 50)
+
+        with patch('time.time', return_value=100), set_stats_on_update(pet, *(100,)*5):
+            assert pet.bring_to('hike') == 'test Moana hike'
+            expect_stats(pet, 90, 80, 70, 60, 50)
+
+
+    def test_bring_to_update_maxed_stats(self, pet: Pet):
+        with set_stats_on_update(pet, *(90,)*5):
+            assert pet.bring_to('park') == 'test Moana park'
+            expect_stats(pet, *(100,)*5)
